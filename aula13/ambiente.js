48e199b193efa767a11ceafe1dbbf88466b6c625
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2023-09-25 21:35:34 -0300

    Criei aula13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+
+var contador = 0
+var contador2 = 0
+while (contador < 10) {
+    console.log(` while ${contador}`)
+    contador++
+}
+
+do {
+    console.log(`do ${contador2}`)
+    contador2++
+} while (contador2 <10)
---
+++ aula14/ambiente.js
@@ -0,0 +1,9 @@
+var c = 1
+while (c <= 5){
+    console.log(`while ${c}`)
+    c++
+}
+
+for (c ; c<=10 ; c++){
+    console.log(`for ${c}`)
+}
