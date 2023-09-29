d4d20e76d185057191c49c4abf83e13fef1a7061
Jhonas de Lima Barbosa <136918301+JhonasLima@users.noreply.github.com>
2023-09-29 19:37:18 -0300

    Aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8 , 4]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posição`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos =  num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
